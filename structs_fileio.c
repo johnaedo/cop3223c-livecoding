c34c569462cea4c9a814ba1fd26d1e2426b6d298
John Aedo <johnaedo@users.noreply.github.com>
2025-07-07 16:29:22 +0000

    Updates for 07-07-2025

--- structs_fileio.c
+++ structs_fileio.c
@@ -2,31 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct book {
+typedef struct {
     int id;
     char title[100];
     char authorFirstName[100];
     char authorLastName[100];
     char publisher[100];
     float price;
-};
+} book_t;
 
-typedef struct book book_t;
+typedef enum {
+    FALSE,
+    TRUE
+} BOOL;
 
-
-book_t *book1;
-book_t *book2;
-
-void printBook(book_t *myBook) {
-    printf("Author: %s %s\n", myBook->authorFirstName, myBook->authorLastName);
-    printf("Title: %s\n", myBook->title);
-    printf("Publisher: %s\n", myBook->publisher);
-    printf("Price: %f\n", myBook->price);
-    printf("ID: %d\n", myBook->id);   
+BOOL hasPublisher(char *publisher) {
+    if (strlen(publisher) == 0) {
+        return FALSE;
+    } else {
+        return TRUE;
+    }    
 }
 
-book_t * createBook(int id, char authorFirstName[100], char authorLastName[100], char title[100], char publisher[100], float price) {
-    book_t *tmpBook;
+void printBook(book_t* myBook) {
+    printf("Author: %s %s\n", myBook->authorFirstName, myBook->authorLastName);
+    printf("Title: %s\n", myBook->title);
+    if (hasPublisher(myBook->publisher)) {
+        printf("Publisher: %s\n", myBook->publisher);
+    }
+    else {
+        printf("Publisher: Unknown\n");
+    };
+    printf("Price: %f\n", myBook->price);
+    printf("ID: %d\n", myBook->id);
+}
+
+book_t* createBook(int id, char authorFirstName[100], char authorLastName[100], char title[100], char publisher[100], float price) {
+    book_t* tmpBook;
     tmpBook = malloc(sizeof(book_t));
     strcpy(tmpBook->authorFirstName, authorFirstName);
     strcpy(tmpBook->authorLastName, authorLastName);
@@ -37,19 +49,26 @@ book_t * createBook(int id, char authorFirstName[100], char authorLastName[100],
     return tmpBook;
 }
 
-void saveBook(FILE *fp, book_t *myBook) {
+void saveBook(FILE* fp, book_t* myBook) {
     fprintf(fp, "%d,%s,%s,%s,%s,%0.2f\n", myBook->id, myBook->authorFirstName, myBook->authorLastName, myBook->title, myBook->publisher, myBook->price);
 }
 
 int main(void) {
-    FILE *fp = fopen("bookdb.txt", "w");
+    book_t* book1;
+    book_t* book2;
+    book_t* book3;
+    FILE* fp = fopen("bookdb.txt", "w");
     book1 = createBook(100, "Arthur C.", "Clarke", "Foundation", "Random House", 17.99);
     book2 = createBook(101, "Orson Scott", "Card", "Ender's Game", "Baen", 22.99);
+    book3 = createBook(102, "H.G.", "Wells", "The Time Machine", "", 12.99);
     printBook(book1);
     printBook(book2);
+    printBook(book3);
     saveBook(fp, book1);
     saveBook(fp, book2);
+    saveBook(fp, book3);
     free(book1);
     free(book2);
-    fclose(fp);    
+    free(book3);
+    fclose(fp);
 }
